3999368cc1694a6fc058745d210339d6bc5f302c
esunker <157544893+esunker@users.noreply.github.com>
2024-11-05 17:45:10 +0000

    formating

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.PriorityQueue;
+
 public class PriorityQueueLab {
     public static void main(String[] args){
-  File file= new File("RandIntegers.txt");
-  PriorityQueue queue= new PriorityQueue<Integer>();
-  Scanner scan;
-  try{
-    scan= new Scanner(file);
-    while(scan.hasNextLine()){
-  queue.add(scan.nextInt());
-  scan.nextLine();
-  }
-  System.out.println(queue);
-  }catch(FileNotFoundException e){
-    System.out.println("No file");
-  }
-  
-}
+        File file= new File("RandIntegers.txt");
+        PriorityQueue queue= new PriorityQueue<Integer>();
+        Scanner scan;
+        try{
+            scan= new Scanner(file);
+            while(scan.hasNextLine()){
+                queue.add(scan.nextInt());
+                scan.nextLine();
+            }
+            System.out.println(queue);
+        }catch(FileNotFoundException e){
+            System.out.println("No file");
+        }
+    }
 }
